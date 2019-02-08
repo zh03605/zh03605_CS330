818849e3f5b543ba96f4d72f070128c8fed08984
zh03605 <34834106+zh03605@users.noreply.github.com>
2019-02-08 18:31:04 +0500

    Add files via upload

---
+++ design/DFF.v
@@ -0,0 +1,17 @@
+module DFF
+  (
+    input c,r,
+    input d,
+    
+    output reg q
+  );
+  
+    always @ (posedge r or posedge c)
+      begin 
+          if (r)
+              q <= 1'b0;
+          else
+              q <= d;
+      end 
+      
+endmodule 
